88de24ad8aa9527d54c2632b2b1794897735d950
mrarthurwhite <mister.arthur.white@gmail.com>
2021-10-11 16:54:46 -0400

    some images

---
+++ src/business/logic/test/subtract/SubtractMethodTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package business.logic.test.subtract;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import business.logic.Calculate;
+
+/**
+ * @author Arthur
+ *
+ */
+class SubtractMethodTest {
+
+	/**
+	 * Test method for {@link business.logic.Calculate#subtract(int, int)}.
+	 */
+	@Test
+	void testSubtract() {
+		Calculate c= new Calculate();
+		assertEquals(1, c.subtract(2,1));
+	}
+
+}
